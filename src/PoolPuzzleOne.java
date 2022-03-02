937d23cc59ba14e9932ba7c1d27ee020f0918e7e
nadir <florindaniel.birsan@gmail.com>
2022-03-02 11:11:13 +0200

    full resolve

--- src/PoolPuzzleOne.java
+++ src/PoolPuzzleOne.java
@@ -1,28 +1,27 @@
-public class PoolPuzzleOne {
-    public static void main(String[] args) {
-
-                int x = 0;
-                while ( x < 3 ) {
-
-                    if ( x < 1 ) {
-                        System.out.print("a");
-                    }
-                    System.out.print(" ");
-
-                    if ( x < 1 ) {
-                        System.out.print("noise");
-                    }
-
-                    if ( x == 1 ) {
-                        System.out.print("annoys");
-                    }
-                    if ( x > 1 ) {
-                        System.out.print("an");
-                    }
-                    System.out.println("");
-                    x++;
-                }
+class PoolPuzzleOne {
+    public static void main(String [] args) {
+        int x = 0;
+        while ( x < 4 ) {
+            System.out.print("a");
+            if ( x < 1 ) {
+                System.out.print(" ");
             }
+            System.out.print("n");
+            if ( x > 1 ) {
+                System.out.print(" oyster");
+                x = x + 2;
+            }
+            if ( x == 1 ) {
+                System.out.print("noys");
+            }
+            if ( x < 1 ) {
+                System.out.print("oise");
+            }
+            System.out.println("");
+            x = x + 1;
         }
+    }
+}
+
 
 
